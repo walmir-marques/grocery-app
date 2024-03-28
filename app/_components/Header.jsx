26f1395602164e8ffb31af212679ca842d3c6369
Walmir Marques Filho <151250830+walmir-marques@users.noreply.github.com>
2024-03-28 15:30:28 -0300

    dropdown menu from database

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
@@ -9,8 +11,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
+import getCategory from "../_utils/GlobalApi";
 
 function Header() {
+  const [categoryList, setCategoryList] = useState([]);
+
+  const getCategoryList = () => {
+    getCategory().then((response) => {
+      setCategoryList(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
   return (
     <div className="flex justify-between p-5 shadow-md">
       <div className="flex items-center gap-8">
@@ -24,18 +40,32 @@ function Header() {
             </h2>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+            <DropdownMenuLabel>Browse Categories</DropdownMenuLabel>
+            {categoryList.map((category) => (
+              <DropdownMenuItem
+                key={category.id}
+                className="flex gap-4 items-center cursor-pointer"
+              >
+                <Image
+                  src={
+                    process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+                    category.attributes?.icon?.data[0]?.attributes?.url
+                  }
+                  alt="icon"
+                  width={30}
+                  height={30}
+                  unoptimized={true}
+                />
+                <h2 className="text-lg">{category.attributes?.name}</h2>
+              </DropdownMenuItem>
+            ))}
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
 
         <div className="md:flex gap-3 items-center border rounded-full p-2 px-5 hidden">
           <Search />
-          <input type="text" placeholder="search" className="outline-none" />
+          <input type="text" placeholder="Search" className="outline-none" />
         </div>
       </div>
       <div className="flex gap-5 items-center">
--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,3 +3,14 @@ const { default: axios } = require("axios");
 const axiosClient = axios.create({
   baseURL: "http://localhost:1337/api",
 });
+
+const getCategory = async () => {
+  try {
+    const response = await axiosClient.get("/Categories?populate=*");
+    return response.data; // Retorna os dados da resposta da requisição
+  } catch (error) {
+    throw error; // Lança o erro caso ocorra algum problema na requisição
+  }
+};
+
+export default getCategory;
